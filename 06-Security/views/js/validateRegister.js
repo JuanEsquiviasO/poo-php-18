9138924a97b570aa26f541b0aded5105fec1cd09
Juan Esquivias <juanesquiviasotazu@gmail.com>
2018-03-27 15:12:57 -0500

    Validate user with javascript and regular expressions

--- 06-Security/views/js/validateRegister.js
+++ 06-Security/views/js/validateRegister.js
@@ -1,11 +1,28 @@
 // coment
 function validateRegister() {
-	var usuario = document.querySelector('#usuarioRegistro').value;
-	console.log('usuario', usuario);
-	var password = document.querySelector('#passwordRegistro').value;
-	console.log('password', password);
-	var email = document.querySelector('#emailRegistro').value;
-	console.log('email', email);
+	var usuario = document.querySelector('#usuarioRegistro').value
+	// console.log('usuario', usuario)
+	var password = document.querySelector('#passwordRegistro').value
+	// console.log('password', password)
+	var email = document.querySelector('#emailRegistro').value
+	// console.log('email', email);
+	
+	// Valid user with javascript and regular expressions
+	if (usuario != "") {
+		var caracteres = usuario.length
+		var expresion = /^[a-zA-Z0-9]*$/
 
-return false;
+		if (caracteres > 6) {
+			document.querySelector("label[for='usuarioRegistro']").innerHTML += "<br>Write less than 6 characters."
+			return false
+		}
+
+		if (!expresion.test(usuario)) {
+			document.querySelector("label[for='usuarioRegistro']").innerHTML += "<br>Do not write special characters."
+			return false
+		}
+
+	}
+
+return true
 }
