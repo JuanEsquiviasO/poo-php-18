99182922e84c3c58a987fb17d511cee1859a2d1d
Juan Esquivias <juanesquiviasotazu@gmail.com>
2018-04-10 13:50:16 -0500

    finished validation from the server side

--- 08-CMS/cms-mvc/backend/controllers/ingreso.php
+++ 08-CMS/cms-mvc/backend/controllers/ingreso.php
@@ -2,16 +2,40 @@
 
 class Ingreso {
 	public function ingresoController () {
-		$datosController = array("usuario"=>$_POST["usuarioIngreso"],
-														 "password"=>$_POST["passwordIngreso"]);
-		
-		$respuesta = IngresoModels::ingresoModel($datosController);
+		if(preg_match('/^[a-zA-Z0-9]+$/', $_POST["usuarioIngreso"]) &&
+			 preg_match('/^[a-zA-Z0-9]+$/', $_POST["passwordIngreso"])) {
 
-		if ($respuesta["usuario"] == $_POST["usuarioIngreso"] && $respuesta["password"] == $_POST["passwordIngreso"]) {
-			header("location:index.php?action=inicio");
-		}
-		else {
-			echo '<div class="alert alert-danger">Error when accessing</div>';
+				#$encriptar = crypt($_POST["passwordIngreso"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+
+			$datosController = array("usuario"=>$_POST["usuarioIngreso"],
+															 "password"=>$_POST["passwordIngreso"]);
+			$respuesta = IngresoModels::ingresoModel($datosController, "usuarios");
+
+			$intentos = $respuesta["intentos"];
+			$usuarioActual = $_POST["usuarioIngreso"];
+			$maximoIntentos = 2;
+
+			if ($intentos < $maximoIntentos) {
+				if ($respuesta["usuario"] == $_POST["usuarioIngreso"] && $respuesta["password"] == $_POST["passwordIngreso"]) {
+					$intentos = 0;
+					$datosController = array("usuarioActual"=>$usuarioActual, "actualizarIntentos"=>$intentos);
+					$respuestaActualizarIntentos = IngresoModels::intentosModel($datosController, "usuarios");
+					header("location:index.php?action=inicio");
+				}
+				else {
+					++$intentos;
+					$datosController = array("usuarioActual"=>$usuarioActual, "actualizarIntentos"=>$intentos);
+					$respuestaActualizarIntentos = IngresoModels::intentosModel($datosController, "usuarios");
+					echo '<div class="alert alert-danger">Error when accessing</div>';
+				}
+			}
+
+			else {
+				$intentos = 0;
+				$datosController = array("usuarioActual"=>$usuarioActual, "actualizarIntentos"=>$intentos);
+				$respuestaActualizarIntentos = IngresoModels::intentosModel($datosController, "usuarios");
+				echo '<div class="alert alert-danger">Has failed three times</div>';
+			}
 		}
 	}
 }
--- 08-CMS/cms-mvc/backend/models/ingreso.php
+++ 08-CMS/cms-mvc/backend/models/ingreso.php
@@ -3,16 +3,29 @@
 require_once "conexion.php";
 
 class IngresoModels{
-	public function ingresoModel($datosModel) {
-		$stmt = Conexion::conectar()->prepare("SELECT usuario, password FROM usuarios WHERE usuario = :usuario AND password = :password");
+	public function ingresoModel($datosModel, $tabla) {
+		$stmt = Conexion::conectar()->prepare("SELECT usuario, password, intentos FROM $tabla WHERE usuario = :usuario");
 
-		$stmt -> bindParam(":usuario", $datosModel["usuario"]);
-		$stmt -> bindParam(":password", $datosModel["password"]);
+		$stmt -> bindParam(":usuario", $datosModel["usuario"], PDO::PARAM_STR);
 
 		$stmt -> execute();
 
 		return $stmt -> fetch();
 
 		$stmt -> close();
+	}
+
+	public function intentosModel($datosModel, $tabla) {
+		$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET intentos = :intentos WHERE usuario = :usuario");
+
+		$stmt -> bindParam(":intentos", $datosModel["actualizarIntentos"], PDO::PARAM_INT);
+		$stmt -> bindParam(":usuario", $datosModel["usuarioActual"], PDO::PARAM_STR);
+
+		if($stmt -> execute()) {
+			return "success";
+		}
+		else {
+			return "error";
+		}
 	}
 }
